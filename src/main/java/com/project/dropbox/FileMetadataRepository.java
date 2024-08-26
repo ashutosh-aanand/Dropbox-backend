05a9e7629dbf47d882629da8799dc96397c441c4
Ashutosh Anand <ashutosh.aanand@gmail.com>
2024-08-26 18:57:33 +0530

    created FileMetadataRepository

---
+++ src/main/java/com/project/dropbox/FileMetadataRepository.java
@@ -0,0 +1,5 @@
+package com.project.dropbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, String> {}
