e6b0f64067e708d1b2d97de6c3c58904fc774f8e
Ashutosh Anand <ashutosh.aanand@gmail.com>
2024-08-26 18:35:18 +0530

    created FileMetaData class

---
+++ src/main/java/com/project/dropbox/FileMetadata.java
@@ -0,0 +1,22 @@
+package com.project.dropbox;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name = "files")
+public class FileMetadata {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String type;
+    private long size;
+    private String path;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+}
