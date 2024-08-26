b7107df23c3d20a3ec623051805dd3fd0d66a464
Ashutosh Anand <ashutosh.aanand@gmail.com>
2024-08-26 18:29:36 +0530

    created /upload route, used FileStorageService. working

--- src/main/java/com/project/dropbox/FileController.java
+++ src/main/java/com/project/dropbox/FileController.java
@@ -1,15 +1,29 @@
 package com.project.dropbox;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/files")
+@RequiredArgsConstructor
 public class FileController {
+
+    private final FileStorageService fileStorageService;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        try {
+            return fileStorageService.storeFile(file);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 }
